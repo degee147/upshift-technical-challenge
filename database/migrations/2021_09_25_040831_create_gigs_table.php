c4799eb8415e57dcfdbb789849681b6376898069
Ken Waribo <degee147@gmail.com>
2021-09-25 05:24:22 +0100

    database migrations

---
+++ database/migrations/2021_09_25_040831_create_gigs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gigs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained('companies');
+            $table->string('name');
+            $table->text('description');
+            $table->string('timestamp_start');
+            $table->string('timestamp_end');
+            $table->integer('number_of_positions');
+            $table->double('pay_per_hour', 8, 2);
+            $table->boolean('posted')->default(0);
+            $table->enum('status', ['Not started', 'Started', 'Finished']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gigs');
+    }
+}
